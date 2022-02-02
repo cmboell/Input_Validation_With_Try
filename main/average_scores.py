fd00978d950f21ddc25d896eaeb9370eb2797e4b
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-02-02 16:58:55 -0600

    edited some notes

--- main/average_scores.py
+++ main/average_scores.py
@@ -1,15 +1,15 @@
 """
-Assignment name: Basic Input and Format Output Assignment
+Assignment name: Input Validation with Try Assignment
 Program: average_scores.py
 Author: Colby Boell
-Last date modified: 01/19/2022
+Last date modified: 02/02/2022
 
 The purpose of this program is to prompt user for their first name, last name, age
 and 3 different test score values out of 100. Then take the 3 scores and find the average to store in a variable,
 then print out the user first and last name, age, and average score (formatted 2 decimal places) as a
 formatted sentence.
+---With try statements
 """
-
 import constants
 
 # prompt user input for first name / this displays prompt and takes the input
